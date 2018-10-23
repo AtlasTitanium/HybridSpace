bc9d3f15fcbdc5acfacffb9a5ce3effa0f62d8c0
Atlas <AtlasTheTitan77@gmail.com>
2018-10-23 23:01:32 +0200

    Game Prototype
    ye

---
+++ Games/Prototype_LaptopEscapeRoom/Assets/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+	public GameObject SetupCanvas, ComputerLogin, Win, Error;
+
+	public InputField loginPasswordInstantiate;
+	public InputField loginPassword;
+	public Button submitInstantiation;
+	public Button submitLoginPassword;
+
+	public string loginPass;
+
+	void Start(){
+		submitInstantiation.onClick.AddListener(SetParameters);
+        submitLoginPassword.onClick.AddListener(delegate {CheckIfCorrect(0); });
+	}
+
+	void Update(){
+		if(submitLoginPassword.IsActive()){
+			if(Input.GetKeyDown(KeyCode.Return)){
+				CheckIfCorrect(0);
+			}
+		}
+	}
+
+	private void SetParameters(){
+		if(loginPasswordInstantiate.text != ""){
+			loginPass = loginPasswordInstantiate.text;
+			SetupCanvas.SetActive(false);
+			ComputerLogin.SetActive(true);
+		}	
+	}
+
+	private void CheckIfCorrect(int whichCheck){
+		switch (whichCheck){
+			case 0:
+				if(loginPassword.text == loginPass){
+					OpenComputer();
+				} else {
+					loginPassword.text = "";
+					if(!Error.active){
+						Error.SetActive(true);
+					} else {
+						Error.GetComponent<Text>().color = new Color(Random.Range(0.1f,1),Random.Range(0.1f,1),Random.Range(0.1f,1));
+					}
+				}
+				break;
+			default:
+				print("Out of scope");
+				break;
+		}
+	}
+
+	private void OpenComputer(){
+		Debug.Log("Open Computer");
+		ComputerLogin.SetActive(false);
+		Win.SetActive(true);
+	}
+}
