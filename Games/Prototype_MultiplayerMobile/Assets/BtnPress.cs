d002ad1a04d65e6399df937d6ec9044ced0dac17
Atlas <AtlasTheTitan77@gmail.com>
2018-11-20 23:51:16 +0100

    NewTest

---
+++ Games/Prototype_MultiplayerMobile/Assets/BtnPress.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+
+public class BtnPress : MonoBehaviour{
+	public string NextLevel;
+	public GameObject block;
+	
+	public void NextScene(){
+		try {
+			Debug.Log("ChangeScene");
+			block.GetComponent<Renderer>().material.color = Color.green;
+        	SceneManager.LoadScene(NextLevel , LoadSceneMode.Single);
+		}
+		catch (Exception e) {
+			Debug.Log("Error");
+			block.GetComponent<Renderer>().material.color = Color.yellow;
+		} 
+		
+	}
+}
