06307d7fc5bc449be8ce817b6db29584c31507fc
shalaka1110 <40331393+shalaka1110@users.noreply.github.com>
2018-11-22 13:18:52 +0100

    21.11

---
+++ Games/Prototype_MultiplayerMobile/Assets/Wrong_answer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wrong_answer : MonoBehaviour {
+
+    public string NextLevel;
+    public void OnWrong()
+    {
+     
+            Debug.Log("ChangeScene");
+          //  block.GetComponent<Renderer>().material.color = Color.green;
+            SceneManager.LoadScene("Wrong");
+       
+      
+            Debug.Log("Error");
+          //  block.GetComponent<Renderer>().material.color = Color.yellow;
+        
+
+    }
+}
